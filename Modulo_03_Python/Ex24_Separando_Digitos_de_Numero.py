d953a90425875617e4c0d19ba714a626a0ba516a
Antonio Kleberson <klebersonsilva862@gmail.com>
2023-08-09 22:42:34 -0300

    Update Ex24
    Faca um programa que leia um numero de 0 a 9999 e mostre na tela cada um dos digitos separados

--- Modulo_03_Python/Ex24_Separando_Digitos_de_Numero.py
+++ Modulo_03_Python/Ex24_Separando_Digitos_de_Numero.py
@@ -1,8 +1,13 @@
 #Faca um programa que leia um numero de 0 a 9999 e mostre na tela cada um dos digitos separados
 
-numero = str(input("Digite o numero: "))
+numero = int(input("Digite o numero: "))
 
-print('Unidade: {}'.format(numero.split()[3]))
-print('Dezena: {}'.format(numero.split()[2]))
-print('Centena: {}'.format(numero.split()[1]))
-print('Milhar: {}'.format(numero.split()[0]))
+u = numero // 1 % 10
+d = numero // 10 % 10
+c = numero // 100 % 10
+m = numero // 1000 % 10
+
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
