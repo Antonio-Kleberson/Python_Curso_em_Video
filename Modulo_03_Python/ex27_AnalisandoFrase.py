0284206ecae33f9ca20829ae8ebddbb625afa1f6
Antonio Kleberson <klebersonsilva862@gmail.com>
2023-08-09 22:56:32 -0300

    Update ex27

--- Modulo_03_Python/ex27_AnalisandoFrase.py
+++ Modulo_03_Python/ex27_AnalisandoFrase.py
@@ -3,14 +3,16 @@
 #Em que posicao ela aparece a primeira vez
 #Em que posicao ela aparece a ultima vez
 
-frase = str(input('Digite uma frase: ').upper()).strip()
+frase = str(input('Digite uma frase: ')).upper().strip()
 
 #Count vai contar quantas vezes a palavra se repete
 print('Quantas vezes aparece a letra A')
 print(frase.count('A'))
 #Vai dizer em que momento apareceu a letra A
 print('Primeira posicao da letra A')
-print(frase.find('A'))
+#+1 para comecar da posicao 1
+print(frase.find('A')+1)
 #Vai indicar o momento que aparece a ultima vez
 print('Ultima posicao da letra A')
-print(frase.rfind('A'))
+#+1 para comecar da posicao 1
+print(frase.rfind('A')+1)
