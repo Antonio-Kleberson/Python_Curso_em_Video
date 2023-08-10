47609d1f8273d5a2d42ef35ac05a7913c7a17ed7
Antonio Kleberson <klebersonsilva862@gmail.com>
2023-08-09 21:40:32 -0300

    add nova questao
    Crie um programa que leia o nome da cidade e diga se ela começa com a palavra "SANTO"

--- Modulo_03_Python/Ex24_Separando_Digitos_de_Numero.py
+++ Modulo_03_Python/Ex24_Separando_Digitos_de_Numero.py
@@ -3,3 +3,6 @@
 numero = str(input("Digite o numero: "))
 
 print('Unidade: {}'.format(numero.split()[3]))
+print('Dezena: {}'.format(numero.split()[2]))
+print('Centena: {}'.format(numero.split()[1]))
+print('Milhar: {}'.format(numero.split()[0]))
---
+++ Modulo_03_Python/ex25_NomeCidade.py
@@ -0,0 +1,6 @@
+#Crie um programa que leia o nome da cidade e diga se ela começa com a palavra "SANTO"
+
+cidade = str(input("Digite o nome da cidade: ")).strip()
+#Strip remove os espaços em branco do início ou no final.
+
+print(cidade[:5].upper() == 'SANTO')
