02e207e6ed2954e5ab00a710beadfc85b1d85a38
Antonio Kleberson <klebersonsilva862@gmail.com>
2023-08-09 22:17:04 -0300

    ex28_AnalisandoNome
    Faca um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o ultimo nome separadamente

---
+++ Modulo_03_Python/ex28_AnalisandoNome.py
@@ -0,0 +1,8 @@
+#Faca um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o ultimo nome separadamente
+
+nome = str(input('Digite o nome da pessoa: '))
+
+#Vai localizar o primeiro nome
+print('Primeiro nome: {}'.format(nome.split()[0]))
+#Vai localizar o ultimo nome
+print('Ultimo nome: {}'.format(nome.split()[-1]))
