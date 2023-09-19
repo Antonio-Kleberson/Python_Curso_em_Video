ba6ae690da8bbf6aa7a4bff1015eabce5cfa0233
Antonio Kleberson <klebersonsilva862@gmail.com>
2023-09-19 10:53:48 -0300

    new function tipo de racao
    Nessas novas funções, é feito uma especifica para cada tipo de ração. Uma para crescimento, terminação, gestação e lactação

--- Projeto_Praticos/calcular_racao.py
+++ Projeto_Praticos/calcular_racao.py
@@ -6,6 +6,62 @@ def consumo_mensal(qnt_animais, kg_racao, kg_saco):
     
     return consumo_diario, consumo_mes, qnt_sacos 
 
+def racao_crescimento(qnt_racao):
+    milho = 0.72
+    soja = 0.24
+    nucleo = 0.04
+    
+    qnt_milho = qnt_racao * milho
+    qnt_soja = qnt_racao * soja
+    qnt_nucleo = qnt_racao * nucleo
+    
+    qnt_total = qnt_milho + qnt_soja + qnt_nucleo
+    
+    return qnt_milho, qnt_soja, qnt_nucleo, qnt_total
+
+def racao_terminacao(qnt_racao):
+    milho = 0.76
+    soja = 0.20
+    nucleo = 0.04
+    
+    qnt_milho = qnt_racao * milho
+    qnt_soja = qnt_racao * soja
+    qnt_nucleo = qnt_racao * nucleo
+    
+    qnt_total = qnt_milho + qnt_soja + qnt_nucleo
+    
+    return qnt_milho, qnt_soja, qnt_nucleo, qnt_total
+
+def racao_gestacao(qnt_racao):
+    milho = 0.57
+    soja = 0.09
+    trigo = 0.30
+    nucleo = 0.04
+    
+    qnt_milho = qnt_racao * milho
+    qnt_soja = qnt_racao * soja
+    qnt_trigo = qnt_racao * trigo
+    qnt_nucleo = qnt_racao * nucleo
+    
+    qnt_total = qnt_milho + qnt_soja + qnt_trigo + qnt_nucleo
+    
+    return qnt_milho, qnt_soja, qnt_trigo, qnt_nucleo, qnt_total
+
+def racao_lactacao(qnt_racao):
+    milho = 0.59
+    soja = 0.24
+    trigo = 0.09
+    nucleo = 0.04
+    
+    qnt_milho = qnt_racao * milho
+    qnt_soja = qnt_racao * soja
+    qnt_trigo = qnt_racao * trigo
+    qnt_nucleo = qnt_racao * nucleo
+    
+    qnt_total = qnt_milho + qnt_soja + qnt_trigo + qnt_nucleo
+    
+    return qnt_milho, qnt_soja, qnt_trigo, qnt_nucleo, qnt_total
+
 # Path: Projeto_Praticos/calcular_racao.py
 
 def main():
@@ -18,12 +74,18 @@ def main():
     consumo = consumo_mensal(*valores)
     
     consumo_diario, consumo_mes, qnt_sacos = consumo_mensal(*valores)
+
+    qnt_milho, qnt_soja, qnt_nucleo, qnt_total = racao_crescimento(consumo_mes)
     
     print('------------------------------------------------------')
     print(f'O consumo diário foi de: {consumo_diario} kg por dia.')
     print(f'O consumo mensal foi de: {consumo_mes} kg por mês.')
     print(f'Quantidade de sacos necessários foi de: {qnt_sacos}')
     print('------------------------------------------------------')
+    print('Quantidade de milho utilizado: {:.2f}'.format(qnt_milho))
+    print('Quantidade de soja utilizado: {:.2f}'.format(qnt_soja))
+    print('Quantidade de nucleo utilizado: {:.2f}'.format(qnt_nucleo))
+    print('Total: {:.2f}'.format(qnt_total))
     
 
 # Verifica se este arquivo está sendo executado diretamente como um programa principal.
