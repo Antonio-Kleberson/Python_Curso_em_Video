35a62c19308c5965622fed7758ab842548231a4f
Antonio Kleberson <klebersonsilva862@gmail.com>
2024-01-23 18:50:48 -0300

    quest resolvida

--- Mundo_2_Python/Modulo_1/ex01_AprovandoEmprestimo.py
+++ Mundo_2_Python/Modulo_1/ex01_AprovandoEmprestimo.py
@@ -1,2 +1,21 @@
-# Escreva um programa para aprovar o emprestimo bancario para a compra de uma casa. O programa vai perguntar o valor da casa, o slaraio do comprador e em quantos anos ele vai pagar.
+# Escreva um programa para aprovar o emprestimo bancario para a compra de uma casa. O programa vai perguntar o valor da casa, o salario do comprador e em quantos anos ele vai pagar.
 # Calcule o valor da prestacao mensal, sabendo que ela nao pode exceder 30% do salario ou entao o emprestimo sera negado.
+
+valor_casa = float(input("Informe o valor da casa: "))
+salario = float(input("Informe o salario do comprador: "))
+qnt_anos = int(input("Informe em quantos anos vai pagar: "))
+
+valor_parcelas = valor_casa / (qnt_anos * 12)
+
+if(valor_parcelas > (salario * 0.3)):
+    print("-----------------------------------------------")
+    print("Emprestimo negado!")
+    print("Valor da parcela muito alta pro salario determinado!")
+    print("-----------------------------------------------")
+else:
+    print("-----------------------------------------------")
+    print("Parabens! Emprestimo concedido!")
+    print("Valor da Casa: {}".format(valor_casa))
+    print("Valor da Parcela: {}".format(valor_parcelas))
+    print("Quantidade de anos para pagar: {} meses ({} anos)".format((qnt_anos * 12), qnt_anos))
+    print("-----------------------------------------------")
--- Mundo_2_Python/Modulo_1/ex04_AlistamentoMilitar.py
+++ Mundo_2_Python/Modulo_1/ex04_AlistamentoMilitar.py
@@ -3,3 +3,20 @@
 # - Se e a hora de se alistar.
 # - Se ja passou do tempo de alistamento.
 # Seu programa tambem devera mostrar o tempo que falta ou que faltou do prazo.
+
+from datetime import date
+
+def alistamento(ano_nasc):
+    ano_atual = date.today().year
+    idade = ano_atual - ano_nasc
+    
+    if idade < 18:
+        print("Voce não tem idade de alistamento!")
+        print(f"Ainda faltam {18 - idade} anos para completar")
+    elif idade == 18:
+        print("Já é hora de fazer o alistamento")
+    else:
+        print("Já passou do tempo de alistamento.")
+
+ano_nasc = int(input("Informe a data de nascimento: "))
+alistamento(ano_nasc)
