8d9936b5a3e9a7d0d062b53b32f34b66061c5908
Antonio Kleberson <klebersonsilva862@gmail.com>
2024-02-29 22:27:27 -0300

    up

--- Mundo_2_Python/Modulo_1/ex02_ConversorBasesNumericas.py
+++ Mundo_2_Python/Modulo_1/ex02_ConversorBasesNumericas.py
@@ -2,3 +2,30 @@
 # 1 para binario
 # 2 para octal
 # 3 para hexadecimal
+
+
+num = int(input("Digite um número inteiro: "))
+
+print('_______________________________________________')
+print('-----------------------------------------------')
+print('Escolha uma das opções para realizar a conversão')
+print('-----------------------------------------------')
+print('| 1 - Converter para Binário     |')
+print('| 2 - Converter para Octal       |')
+print('| 3 - Converter para Hexadecimal |')
+opcao = int(input('Digite a opção desejada para conversão: '))
+print('_______________________________________________')
+
+if opcao == 1:
+    binario = ''
+    while num > 0:
+        resto = num % 2
+        binario = str(resto) + binario
+        num = num // 2
+    print(f'O número em binário é {binario}')
+elif opcao == 2:
+    print(f'O número em octal é {oct(num)}')
+elif opcao == 3:
+    print(f'O número em hexadecimal é {hex(num)}')
+else:
+    print('Opção inválida.')
--- Mundo_2_Python/Modulo_1/ex03_ComparandoNumeros.py
+++ Mundo_2_Python/Modulo_1/ex03_ComparandoNumeros.py
@@ -2,3 +2,13 @@
 # O primeiro valor valor e maior
 # O segundo valor e maior
 # Nao existe valor maior, os dois sao iguais
+
+num1 = int(input('Digite um numero inteiro: '))
+num2 = int(input('Digite um segundo numero inteiro: '))
+
+if num1 > num2:
+    print('O primeiro valor {} é maior'.format(num1))
+elif num2 > num1:
+    print('O segundo valor {} é maior'.format(num2))
+else:
+    print('Os dois valores {} e {} são iguais!'.format(num1, num2))
