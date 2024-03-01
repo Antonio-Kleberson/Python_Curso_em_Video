2946e76752c1a6122dbba58bb6be390a37d7d02e
Antonio Kleberson <klebersonsilva862@gmail.com>
2024-02-29 21:46:38 -0300

    update

--- Mundo_2_Python/Modulo_1/ex05_Media.py
+++ Mundo_2_Python/Modulo_1/ex05_Media.py
@@ -2,3 +2,31 @@
 # - Media abaixo de 5.0: REPROVADO
 # - Media entre 5.0 e 6.9: RECUPERADO
 # - Media acima de 7.0: APROVADO
+
+qnt = int(input("Quantas notas você quer informar: "))
+notas = []
+
+# Adicionando notas ao vetor
+for i in range(qnt):
+    nota = float(input("Informe a {}° nota: \n".format(i+1)))
+    notas.append(nota)
+
+print("\n")
+
+# Media das notas
+media = sum(notas) / qnt
+
+# Imprimindo as notas sem colchetes
+print("Notas:", end=" ")
+for nota in notas:
+    print(nota, end=" | ")
+
+print("\n")
+
+print("MÉDIA: {}".format(media))
+if media < 5:
+    print("\n\nA sua média é {:.1f} e você está REPROVADO.".format(media))
+elif media <= 6.9:
+    print("\n\nA sua média é {:.1f} e você está em RECUPERAÇÃO.".format(media))
+else:
+    print("\n\nA sua média é {:.1f} e você está APROVADO.".format(media))
--- Mundo_2_Python/Modulo_1/ex06_ClassificandoAtletas.py
+++ Mundo_2_Python/Modulo_1/ex06_ClassificandoAtletas.py
@@ -4,3 +4,38 @@
 # - Ate 19 anos: JUNIOR
 # - Ate 20 anos: SENIOR
 # - Acima: MASTER
+
+
+from datetime import datetime
+
+
+def categoria(nasc):
+    data_atual = datetime.now()
+
+    # Calculando a diferença entre a data atual e a data de nascimento
+    dif = data_atual - nasc
+
+    # Obtendo a parte de anos da diferença
+    idade = dif.days // 365
+    
+    print("\n")
+    
+    if idade <= 9:
+        print("CATEGORIA: MIRIM")
+    elif idade <= 14:
+        print("CATEGORIA: INFANTIL")
+    elif idade <= 19:
+        print("CATEGORIA: JUNIOR")
+    elif idade <= 20:
+        print("CATEGORIA: SENIOR")
+    else:
+        print("CATEGORIA: MASTER")
+        
+    print("\n")
+    
+dia = int(input("Informe o dia do nacimento: "))
+mes = int(input("Informe o mes do nacimento: "))
+ano = int(input("Informe o ano do nacimento: "))
+
+nasc = datetime(ano, mes, dia)
+categoria(nasc)
