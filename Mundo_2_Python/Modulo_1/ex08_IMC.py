945d4c2dba781aa6423675961d4da2860fc3903b
Antonio Kleberson <klebersonsilva862@gmail.com>
2024-04-19 12:38:03 -0300

    New Questions

--- Mundo_2_Python/Modulo_1/ex08_IMC.py
+++ Mundo_2_Python/Modulo_1/ex08_IMC.py
@@ -2,5 +2,22 @@
 # - Abaixo de 18.5: ABAIXO DO PESO
 # - Entre 18.5 e 24.9: PESO IDEAL
 # - Acima de 25 ATE 30: ACIMA DO PESO
-# - 30 ate 40: OBSEIDADE
+# - 30 ate 40: OBESIDADE
 # - Acima de 40: OBESIDADE MORBIDA
+
+peso = float(input('Informe o seu  peso em kg: '))
+altura = float(input('Informe a sua altura em metros: '))
+
+#  Cálculo do IMC é dado pela fórmula: (Peso / (Altura²))
+imc = peso /  (altura ** 2)
+
+if(imc < 18.5):
+    print(f'Seu IMC é {imc:.1f}, você está ABAIXO DO PESO')
+elif(imc > 18.5 and imc <= 24.9):
+    print(f'Seu IMC é {imc:.1f}, você está no PESO IDEAL')
+elif(imc > 25 and imc <= 30):
+    print(f'Seu IMC é {imc:.1f}, você está ACIMA DO PESO')
+elif(imc > 30 and imc <= 40):
+    print(f'Seu IMC é {imc:.1f}, você está em OBESIDADE')
+else:
+    print(f'Seu IMC é {imc:.1f}, você está em OBESIDADE MORBIDA')
--- Mundo_2_Python/Modulo_1/ex09_GerenciadorPagamentos.py
+++ Mundo_2_Python/Modulo_1/ex09_GerenciadorPagamentos.py
@@ -3,3 +3,67 @@
 # - A vista no Cartao: 5% de desconto
 # - Em ate 2x no Cartao: Preco normal
 # - 3x ou mais no Cartao: 20% de juros
+
+
+print("Escolha um produto:")
+print("|---------------------------|")
+print("| 1 - Celular    |R$ 1000,00|")
+print("| 2 - TV 32'     |R$ 1500,00|")
+print("| 3 - Geladeira  |R$ 2500,00|")
+print("| 4 - Fogão      |R$ 2100,00|")
+print("| 5 - Computador |R$ 2000,00|")
+print("|---------------------------|")
+opcao = int(input("Digite  o numero da opção: "))
+
+preco = 0
+
+if(opcao == 1):
+    preco = 1000
+elif(opcao == 2):
+    preco = 1500
+elif(opcao == 3):
+    preco = 2500
+elif(opcao == 4):
+    preco = 2100
+elif(opcao == 5):
+    preco = 2000
+else:
+    print("Opção Invalida!!")
+    
+print("Escolha uma opção de pagamento:")
+print("|------------------------------------------------|")
+print("| 1 - Dinheiro ou PIX           |  10% Desconto  |")
+print("| 2 - À vista no cartão         |  5% Desconto   |")
+print("| 3 - Em ate 2x no cartão       |  Sem Desconto  |")
+print("| 4 - Em 3x ou  mais no cartão  |  20 de Juros   |")
+print("|------------------------------------------------|")
+pagamento = int(input("Digite o número da opção: "))
+
+valor = 0
+
+if(pagamento == 1):
+    valor = preco - (preco * 0.1)
+elif(pagamento == 2):
+    valor = preco - (preco * 0.05)
+elif(pagamento == 3):
+    valor = preco 
+elif(pagamento == 4):
+    valor = preco + (preco * 0.2)
+else:
+    print("Opção Invalida!!")
+    
+print("O produto escolhido foi:  ", end=" ")
+if(opcao == 1) :
+    print("Celular)", end=" ")
+elif(opcao == 2):
+    print("TV 32", end=" ")
+elif(opcao == 3):
+    print("Geladeira", end=" ")
+elif(opcao == 4):
+    print("Fogão", end=" ")
+else:
+    print("Computador", end=" ")
+
+print("\nValor do produto: R$ {:.2f}".format(valor))
+
+    
